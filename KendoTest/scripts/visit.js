2b49ebffdfebd63ddf8dc328e88eadddfb567599
Amir Gilboa <amirgilboa@gmail.com>
2014-01-26 11:28:58 -0600

    Changes made by Amir Gilboa (amirgilboa@gmail.com).

--- KendoTest/scripts/visit.js
+++ KendoTest/scripts/visit.js
@@ -3,11 +3,28 @@
         app = global.app = global.app || {};
     
     LoginViewModel = kendo.data.ObservableObject.extend({
+        // variables here
         isLoggedIn: false,
         username: "",
         password: "",
+        visitrequest: {firstname:"", lastname:"", dateofbirth:"", address:"", city:"", postalcode:"", ohip:"", comment:""}
         
         
+        
+        
+        // methods here
+        clearForm: function () {
+            var that = this;
+
+            that.set("username", "");
+            that.set("password", "");
+        },
+        
+        sendRequest: function(){
+            var that = this;
+            
+        
+        }
         
         });
         
